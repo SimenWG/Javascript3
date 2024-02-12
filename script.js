1fcb257d57531a6380f045e99a6558497a525b85
SimenWG <simenwang07@gmail.com>
2024-02-12 12:28:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+//oppgave 1
+/*
+let i = 0
+while (i <= 50) {
+    console.log (i);
+    i++;
+}
+*/
+ 
+//oppgave 2
+/*
+let i = 0;
+while (i <= 100) {
+    console.log(i);
+    i = i + 2;
+}
+*/
+ 
+//oppgave 3
+/*
+let i = 0;
+while (i <= 999) {
+    console.log(i)
+    i = i + 3;
+}
+*/
+ 
+//oppgave 4
+/*
+let i = 0;
+while (i <= 1000) {
+    if (i%3 == 0 && i%5 == 0) {
+        console.log(i);
+    }
+    i++;
+}
+*/
+ 
+//oppgave 5
+/*
+let total = 0;
+let i = 1;
+while (i <= 100) {
+    total += i;
+    i++;
+}
+console.log(total);
+*/
+ 
+//oppgave 6
+/*
+let pyramid = '';
+for (let i = 1; i <= 4; i++) {
+    pyramid += '#';
+    console.log(pyramid);
+}
+*/
+ 
+//oppgave 7
+/*
+let pyramid = '';
+for (let i = 1; i <= 50; i++) {
+    pyramid += '#';
+    console.log(pyramid);
+}
+*/
+
+//oppgave 8
+/*
+var i = 1;
+ 
+while (i <=100) {
+    if (i%3 == 0 && i%5 == 0) {
+        console.log("FizzBuzz");
+    }
+    else if (i%3 == 0) {
+        console.log("Fizz");
+    }
+    else if (i%5 == 0) {
+        console.log("Buzz");
+    }
+    else {console.log(i);
+    }
+    i++;
+}
+*/
